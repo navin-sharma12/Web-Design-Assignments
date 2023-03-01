79c8e01ddd7ec23185b1cb5d304320a15bd96cc8
Navin <sharma.navi@northeastern.edu>
2023-02-28 22:28:30 -0500

    Login Done

--- Assignment 6/Part A/jquery.js
+++ Assignment 6/Part A/jquery.js
@@ -8,8 +8,9 @@ $(document).ready(function () {
     let passwordError = true;
 
     $("#emailid").keyup(function () {
+        let regex = /\w+\.\w+\@northeastern\.edu/;
         let emailidValue = $("#emailid").val();
-        if(emailidValue.length == 0) {
+        if(emailidValue.length == 0 | regex.test(emailidValue) == false) {
             $("#error_emailid").show();
             emailidError = true;
             return false;
@@ -34,8 +35,9 @@ $(document).ready(function () {
     })
     
     $("#password").keyup(function () {
+        let regexPassword = /(?=.*[@#$%&*()!^])(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9]).{8,}/;
         let passwordValue = $("#password").val();
-        if(passwordValue.length == 0) {
+        if(passwordValue.length == 0 | regexPassword.test(passwordValue) == false) {
             $("#error_password").show();
             passwordError = true;
             return false;
@@ -47,7 +49,16 @@ $(document).ready(function () {
     })
 
     $("#submitButton").click(function () {
-        if(usernameError && emailidError && passwordError == true) {
+        if(usernameError | emailidError | passwordError == true) {
+            if(usernameError == true) {
+                $("#error_username").show();
+            }
+            if(emailidError == true) {
+                $("#error_emailid").show();
+            }
+            if(passwordError == true) {
+                $("#error_password").show();
+            }
             return false;
         }
     })
