5a83ada48e93c58cdba6dbdde082789a8062d32e
Navin <sharma.navi@northeastern.edu>
2023-03-28 19:17:29 -0400

    Assignment 9 initiated

--- Assignment 8/app.js
+++ Assignment 8/app.js
@@ -11,12 +11,21 @@ const userRouter = require("./routes/user");
 
 app.use("/user", userRouter);
 
-mongoose.connect(
-  "mongodb://localhost:27017/assignment8?readPreference=primary&appname=MongoDB%20Compass%20Community&ssl=false",
-  { family: 4 },
-  () => {
-    console.log("connected to db, starting app");
-    app.listen(3000);
+// mongoose.connect(
+//   "mongodb://localhost:27017/assignment8?readPreference=primary&appname=MongoDB%20Compass%20Community&ssl=false",
+//   { family: 4 },
+//   () => {
+//     console.log("connected to db, starting app");
+//     app.listen(3000);
+//     console.log("app started");
+//   }
+// );
+mongoose.connect("mongodb://localhost:27017/assignment8?readPreference=primary&appname=MongoDB%20Compass%20Community&ssl=false", { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to MongoDB database');
+    app.listen(8080);
     console.log("app started");
-  }
-);
+  })
+  .catch((err) => {
+    console.log('Error connecting to MongoDB database', err);
+  });
