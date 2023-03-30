5ce1b65d20976ee7214ab3dd032aecfd6e83ad25
Navin <sharma.navi@northeastern.edu>
2023-03-30 00:35:33 -0400

    updated

--- Assignment 9/frontend/src/Login/LoginPage.js
+++ Assignment 9/frontend/src/Login/LoginPage.js
@@ -1,20 +1,42 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from 'react';
 import "../Login/LoginPage.css";
 
-export default function LoginPage({...props}){
-    const navigate = useNavigate();
-    const handleSignIn = () => {
-        props.handleLogin();
-        navigate('/main')
+export default function LoginPage ({ setUser }) {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const loginUser = (e) => {
+        e.preventDefault();
+        fetch("http://localhost:8080/assignment8", {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                email,
+                password,
+            }),
+        })
+        .then((res) => {
+            if (res.status === 200) {
+                res
+                .json()
+                .then((user) => setUser(user))
+                .catch((err) => alert(err));
+            } else {
+                alert("Invalid Credentials");
+            }
+        })
+        .catch((err) => {
+            alert("Invalid email or password");
+        });
     }
     return(
         <div class="container">
             <form id="myForm">
                 <h2>Login</h2>
-                <input type="text" id="emailid" placeholder="Email ID"/><br/>
-                <input type="password" id="password" placeholder="Password"/><br/>
-                <button type="submit" id="submitButton" onClick={handleSignIn}>Login</button>
+                <input type="text" id="emailid" placeholder="Email ID" value={email} onChange={(e) => setEmail(e.target.value)}/><br/>
+                <input type="password" id="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)}/><br/>
+                <button type="submit" id="submitButton" onClick={loginUser}>Login</button>
             </form>
         </div>
     )
